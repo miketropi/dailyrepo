f291de8fbd3ade8257757067ef5c750dbdab90ef
Hiếu Huỳnh <mike.beplus@gmail.com>
2023-06-12 22:04:53 +0700

    Update api/index

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,5 +1,13 @@
 import { request, gql } from 'graphql-request'
 
-export function _Request() {
+export async function _Request() {
+
+}
+
+export async function GET_TOPICS() {
+
+}
+
+export async function GET_POSTS() {
 
 }
